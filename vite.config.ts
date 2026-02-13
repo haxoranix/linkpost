fc999a76054bc554bf91c9dd1c0bea7912df5bf5
ANIMESH H <me@animeshdas.in>
2026-02-14 04:32:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const repoName = 'linkpost';
+const repoName = 'linkpost'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/'
-});
+  base: mode === 'production' ? `/${repoName}/` : '/',
+}))
